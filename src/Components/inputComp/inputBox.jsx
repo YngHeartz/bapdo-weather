7ddb9413b7748b874131d6dab36bb0c853d7e67c
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-01-03 16:17:15 -0500

    cleaned up the code that was not being used to i can start fresh on the functionality

--- src/Components/inputComp/inputBox.jsx
+++ src/Components/inputComp/inputBox.jsx
@@ -2,51 +2,10 @@ import React, { useState, useEffect } from "react";
 
 const Input = () => {
     const [location, setLocation] = useState('');
-    const [weatherData, setWeatherData] = useState(null);
-
-    useEffect(() => {
-        const fetchWeatherData = async () => {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.WEATHER_API_KEY}`);
-            const data = await response.json();
-            setWeatherData(data);
-        };
-
-        if (location) {
-            fetchWeatherData();
-        }
-    }, [location]);
-
-    const handleInputChange = (e) => {
-        setLocation(e.target.value);
-    };
-
-    let imageUrl = '';
-
-    if (weatherData && weatherData.weather && weatherData.weather.length > 0) {
-        const mainWeatherCondition = weatherData.weather[0].main;
-
-        switch (mainWeatherCondition) {
-            case 'Rain':
-                imageUrl = 'rainy-weather.png';
-                break;
-            case 'Wind':
-                imageUrl = 'windy-weather.png';
-                break;
-            case 'Thunderstorm':
-                imageUrl = 'stormy-weather.png';
-                break;
-            case 'Clear':
-                imageUrl = 'night-weather.png';
-                break;
-            default:
-                imageUrl = 'default-weather.png';
-                break;
-        }
-    }
-
+    
     return (
         <div className="flex items-center justify-center mb-12 mt-2 ">
-            <input className="p-2 border rounded shadow-md shadow-dark-600" type="text" value={location} onChange={handleInputChange} placeholder="Enter location. . ." />
+            <input className="p-2 border rounded shadow-md shadow-dark-600" type="text" placeholder="Enter location. . ." />
             {/* <img src={imageUrl} alt="Weather condition" /> */}
         </div>
     );
