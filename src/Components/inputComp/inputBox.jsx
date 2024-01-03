f5e5338bfd78ebb2d57e367b525745ea66f54851
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-01-03 16:50:11 -0500

    added a svg image for user to click and search for location

--- src/Components/inputComp/inputBox.jsx
+++ src/Components/inputComp/inputBox.jsx
@@ -1,12 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Input = () => {
     const [location, setLocation] = useState('');
-    
+
+    const search = () => {
+        const element = document.getElementsByClassName("")
+    }
     return (
-        <div className="flex items-center justify-center mb-12 mt-2 ">
-            <input className="p-2 border rounded shadow-md shadow-dark-600" type="text" placeholder="Enter location. . ." />
-            {/* <img src={imageUrl} alt="Weather condition" /> */}
+        <div className="flex items-center justify-center mb-12 mt-2">
+            <div className="relative">
+                <input
+                    className="p-2 pr-10 border rounded shadow-md shadow-dark-600" 
+                    type="text" 
+                    value={location} 
+                    onChange={(e) => setLocation(e.target.value)} 
+                    placeholder="Enter location. . ." 
+                />
+                <img 
+                    src="src/assets/searchIcon.png" 
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2 w-20 h-8" 
+                    alt=""
+                    onClick={() => {
+                        search()
+                    }}
+                />
+            </div>
         </div>
     );
 }
