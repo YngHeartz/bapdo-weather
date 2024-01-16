cd4889edab1a31451b4d56cfb0b11e459af240a1
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-01-16 18:48:45 -0500

    styled the contact form

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -7,7 +7,7 @@ export function Contact() {
         <div className="text-4xl text-center p-3" id="Contact">
             <h1 className="font-extrabold text-blue-300 mb-3 mt-40">Contact</h1>
 
-            <p className="text-black  mb-96 ml-32 mr-28 leading-relaxed">Need assistance or want to share your feedback with Bapdo Weather? Reach out via email at support@bapdoweather.com, follow us on social media [@BapdoWeather], or call our 24/7 hotline at +1 (555) 123-4567. For mail correspondence, use our address: 123 Weather Avenue, Cityville, WX 12345, United States. We value your input and are here to help!</p>
+            <p className="text-black  mb-96 ml-32 mr-28 leading-relaxed">Need assistance or want to share your feedback with Bapdo Weather? Reach out via email at <span className="text-pink-300">support@bapdoweather.com</span>, follow us on social media <span className="text-pink-300">@BapdoWeather</span>, or call our 24/7 hotline at <span className="text-pink-300">+1 (555) 123-4567</span>. For mail correspondence, use our address: <span className="text-pink-300">123 Weather Avenue, Cityville, WX 12345, United States</span>. We value your input and are here to help!</p>
         </div>
             
         </>
