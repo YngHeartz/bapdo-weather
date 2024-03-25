7342f818b8fb32c98881c9df7f53ade010125ef0
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-03-25 13:39:29 -0400

    new header design

--- src/Components/headerComp/header.jsx
+++ src/Components/headerComp/header.jsx
@@ -5,7 +5,7 @@ import Lottie from 'lottie-react';
 const Headerofsite = () => {
     return (
         <div className="flex flex-col items-center justify-center mt-10 ">
-            <h1 className="font-extrabold text-6xl mb-4 text-blue-300" id='Home'>Bapdo Weather</h1>
+            <h1 className="font-extrabold text-6xl mb-4 bg-pink-200 rounded-xl p-7" id='Home'>Bapdo Weather</h1>
             <Lottie animationData={weather} className='size-80' />
 
         </div>
